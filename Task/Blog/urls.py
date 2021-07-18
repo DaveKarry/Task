0432d367d3f4c1efb868945e0fe8ac601aa0c17d
K <Davekarry1337@gmail.com>
2021-07-18 19:21:15 +0500

    add post model, authorization, selfblog, otherblog

--- Task/Blog/urls.py
+++ Task/Blog/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('users', Users.as_view(), name='userslist'),
+    path('users/<username>', UserBlog.as_view(), name='userblog'),
+    path('myblog', MyBlog.as_view(), name='myblog'),
+    path('createpost', CreatePost.as_view(), name='newpost'),
+
 ]
--- Task/Blog/views.py
+++ Task/Blog/views.py
@@ -1,10 +1,82 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+
+from Blog.forms import PostForm
+from Blog.models import Post
 
 
 class Index(View):
     def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            posts = Post.objects.filter(
+                author=self.request.user
+            ).order_by('-posted_date')
+            return render(self.request, 'blog/index.html', {'posts': posts})
+        else:
+            message = "Войдите что-бы просмотреть свои посты!"
+            return render(self.request, 'blog/index.html', {'message': message})
+
+
+class Users(View):
+    def get(self, *args, **kwargs):
         User = get_user_model()
         users = User.objects.all()
-        return render(self.request, 'users/index.html')
+        return render(self.request, "blog/users.html", {'users': users})
+
+
+class MyBlog(View):
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            posts = Post.objects.filter(
+                author=self.request.user
+            ).order_by('-posted_date')
+            form = PostForm()
+            context = {'form': form, 'posts': posts}
+            return render(self.request, "blog/myblog.html", context)
+        else:
+            message = "Войдите что-бы просмотреть свои посты!"
+            return render(self.request, 'blog/myblog.html', {'message': message})
+
+    def post(self, *args, **kwargs):
+        form = PostForm(self.request.POST, self.request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = self.request.user
+            post.save()
+            return redirect('myblog')
+        else:
+            return HttpResponseRedirect('createtovar')
+
+
+class CreatePost(View):
+    def get(self, *args, **kwargs):
+        form = PostForm()
+        context = {'form': form}
+        return render(self.request, 'blog/createpost.html', context)
+
+    def post(self, *args, **kwargs):
+        messages = False
+        form = PostForm(self.request.POST, self.request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = self.request.user
+            post.save()
+            return redirect('myblog')
+        else:
+            messages.info(self.request, 'Какая-то ошибка! Прверьте поля или попробуйте снова!')
+            return HttpResponseRedirect('createpost')
+
+
+class UserBlog(View):
+    def get(self, *args, **kwargs):
+        user = get_object_or_404(User, username=kwargs['username'])
+
+        posts = Post.objects.filter(
+            author=user
+        ).order_by('-posted_date')
+        context = {'user': user, 'posts': posts}
+        return render(self.request, 'blog/userblog.html', context)
+
