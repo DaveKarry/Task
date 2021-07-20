c5cb2fbd8c837dc3c0255cfeb98ea526b3093c3e
K <Davekarry1337@gmail.com>
2021-07-20 13:22:36 +0500

    docker fix

--- Task/Task/settings.py
+++ Task/Task/settings.py
@@ -129,5 +129,5 @@ EMAIL_HOST_USER = "Davekarry1337@gmail.com"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "Kiry69076907"
+EMAIL_HOST_PASSWORD = "secretpassword"
 
